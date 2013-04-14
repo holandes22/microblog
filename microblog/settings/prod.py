099883d3e066394c1294e0a35811d46dc35e2b5c
Pablo Klijnjan <pablokl@il.ibm.com>
2013-04-14 18:12:58 +0300

    Adding index.html and heroku app to known hosts

--- microblog/settings/prod.py
+++ microblog/settings/prod.py
@@ -2,6 +2,7 @@ import dj_database_url
 from microblog.settings.base  import *
 
 DATABASES['default'] =  dj_database_url.config()
+ALLOWED_HOSTS = ['glacial-forest-3650.herokuapp.com']
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
