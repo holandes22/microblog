f3abe94df360e1a3f753bbe162c8b82f49040c35
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-04-26 19:43:25 +0300

    Allow to devlope locally with heroku settings

--- microblog/settings/prod.py
+++ microblog/settings/prod.py
@@ -1,8 +1,11 @@
 import dj_database_url
 from microblog.settings.base  import *
 
-DATABASES['default'] =  dj_database_url.config()
-ALLOWED_HOSTS = ['glacial-forest-3650.herokuapp.com']
+# Allow local dev locally
+# http://stackoverflow.com/questions/14795824/improperlyconfiguredsettings-databases-is-improperly-configured-error-when
+DATABASES['default'] =  dj_database_url.config(default='postgres://vagrant:vagrant@localhost/microblog')
+
+ALLOWED_HOSTS = ['localhost', 'glacial-forest-3650.herokuapp.com']
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
